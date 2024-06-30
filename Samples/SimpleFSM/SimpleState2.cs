c94ab5d629d8e9275a179eb6d56a77f1d9f10320
jafarovja <jafarovja@yandex.ru>
2024-06-30 17:38:37 +0700

    разработал пример простой машины состояний (SimpleStateMachine)

---
+++ Samples/SimpleFSM/SimpleState2.cs
@@ -0,0 +1,29 @@
+﻿using Shafir.FSM;
+using UnityEngine;
+
+public class SimpleState2 : IState
+{
+    private Rigidbody _rigidbody;
+    private Collider _collider;
+
+    public SimpleState2
+    (
+        Rigidbody rigidbody,
+        Collider collider
+    )
+    {
+        _rigidbody = rigidbody;
+        _collider = collider;
+    }
+
+    public void Enter()
+    {
+        _collider.enabled = true;
+        _rigidbody.isKinematic = false;
+    }
+
+    public void Exit()
+    {
+
+    }
+}
---
+++ Samples/SimpleFSM/SimpleTestEntryPoint.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Shafir.FSM;
+
+public class SimpleTestEntryPoint : MonoBehaviour
+{
+    [SerializeField] private Transform _transform;
+    [SerializeField] private Transform _goalPosTransform;
+    [SerializeField] private ParticleSystem _particle;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Collider _collider;
+
+    private SimpleState1 _state1;
+    private SimpleState2 _state2;
+    private SimpleStateMachine _stateMachine;
+
+    private void Start()
+    {
+        _state1 = new SimpleState1
+        (
+            _transform,
+            _rigidbody,
+            _collider,
+            _particle,
+            _goalPosTransform.position
+        );
+
+        _state2 = new SimpleState2
+        (
+            _rigidbody,
+            _collider
+        );
+
+        _stateMachine = new SimpleStateMachine();
+
+        _stateMachine.ChangeState(_state1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _stateMachine.ChangeState(_state1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _stateMachine.ChangeState(_state2);
+        }
+    }
+}
