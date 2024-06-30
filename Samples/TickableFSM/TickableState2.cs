e406784811d8307794cbfb4121d1c00564c37ac6
jafarovja <jafarovja@yandex.ru>
2024-06-30 17:47:12 +0700

    разработал пример обновялемой машины состояний (TickableStateMachine)

---
+++ Samples/TickableFSM/TickableState2.cs
@@ -0,0 +1,36 @@
+﻿using Shafir.FSM;
+using UnityEngine;
+
+public class TickableState2 : ITickableState
+{
+    private Rigidbody _rigidbody;
+    private Collider _collider;
+
+    private float _rotateStrength = 100;
+
+    public TickableState2
+    (
+        Rigidbody rigidbody,
+        Collider collider
+    )
+    {
+        _rigidbody = rigidbody;
+        _collider = collider;
+    }
+
+    public void Enter()
+    {
+        _collider.enabled = true;
+        _rigidbody.isKinematic = false;
+    }
+
+    public void Tick()
+    {
+        _rigidbody.AddTorque(Vector3.forward * _rotateStrength);
+    }
+
+    public void Exit()
+    {
+
+    }
+}
